e6db29672ba48577a346b6c7d6c5a8375ce0e22e
신정아 <shinjeonga@twentyoz.kr>
2022-06-08 18:26:04 +0900

    oauth2 인증 서버 config

--- src/main/java/group/oauth/configure/SecurityConfig.java
+++ src/main/java/group/oauth/configure/SecurityConfig.java
@@ -15,6 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
+//클라이언트와 토큰 관리는 Spring Security OAuth 모듈이 담당하지만 사용자 관리는 Spring Security의 몫이다
+//따라서 사용자를 관리하는 spring security는 securityConfig에 따로 설정을 해놓았다
+
+//인증 요청을 처리하려면 사용자 저장소에 접근할 수 있어야 한다.
 
 @Configuration
 @EnableWebSecurity
@@ -48,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     //AuthenticationManagerBuilder를 통해 인증 객체를 만들 수 있도록 제공하고 있습니다
-    //애초에 .authenticationManager()는 인자로 받은 Authentication이 유효한 인증인지 확인하고, "Authentication" 객체를 리턴
+    //즉 메모리에 사용자 정보를 인증 객체로 만들어 등록하는 것
     auth.userDetailsService(userDetailsService)
         .passwordEncoder(encoder());
   }
---
+++ src/main/java/group/oauth/configure/web/WebSecurityAuthorizationConfiguration.java
@@ -0,0 +1,57 @@
+package group.oauth.configure.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+//Oauth 개념 중 AuthorizationServer에 대한 설정
+@Configuration
+@RequiredArgsConstructor
+@EnableAuthorizationServer // oauth 서버에 필요한 기본 설정을 셋팅
+public class WebSecurityAuthorizationConfiguration extends AuthorizationServerConfigurerAdapter {
+
+  private final TokenStore tokenStore;
+
+  private final AuthenticationManager authenticationManager;
+
+  private final PasswordEncoder passwordEncoder;
+
+  private final UserDetailsService userDetailsService;
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception { // API의 요청 클라이언트 정보를 설정
+    clients.inMemory() //inMemory()는 클라이언트(=나. 개발자. 애플리케이션) 정보를 메모리에 저장 개발 환경에 적합 //jdbc()는 데이터베이스에 저장한다. 운영 환경에 적합하다.
+        .withClient("jeong")//withClient()로 client_id 값을 설정한다. secret()은 client_secret 값을 설정한다.
+        .secret(passwordEncoder.encode("jeong123"))
+        .authorizedGrantTypes("password", "refresh_token") //authorizedGrantTypes()는 grant_type(access_token을 획득하기 위한 4가지 인증 방법) 값을 설정한다. 복수개를 저장할 수 있다.
+        .scopes("read", "write") //scopes()는 scope 값을 설정한다.
+        .accessTokenValiditySeconds(60 * 60) //이 클라이언트로 발급된 토큰의 시간 (단위:초)
+        .refreshTokenValiditySeconds(60 * 60 * 60) //// refresh token 만료시간
+        .and()
+        .withClient("hyun")//클라이언트를 한 명 더 등록
+        .secret(passwordEncoder.encode("hyun123"))
+        .authorizedGrantTypes("password", "refresh_token")
+        .scopes("read", "write")
+        .accessTokenValiditySeconds(60 * 60 * 60)
+        .refreshTokenValiditySeconds(60 * 60 * 60);
+  }
+
+  //클라이언트와 토큰 관리는 Spring Security OAuth 모듈이 담당하지만 사용자 관리는 Spring Security의 몫이다
+  //따라서 사용자를 관리하는 spring security는 securityConfig에 따로 설정을 해놓았다
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {//인증, 토큰엔드포인트, 토큰 서비스를 정의 할 수 있다.
+    //endpoint는 authorization server가 동작하는 포인트
+    endpoints.tokenStore(tokenStore) // 즉, 인증 서버에 token store를 등록
+        .authenticationManager(authenticationManager) // 인증 서버에 인증 매니저를 등록
+        .userDetailsService(userDetailsService); // 인증 서버에 userDetailsService를 등록
+  }
+
+}
