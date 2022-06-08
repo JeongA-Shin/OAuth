486494e2dbe692357c46eb6e6d657894c5f09c1f
신정아 <shinjeonga@twentyoz.kr>
2022-06-08 14:45:05 +0900

    project init and entities

---
+++ src/main/java/group/oauth/feature/model/User.java
@@ -0,0 +1,47 @@
+package group.oauth.feature.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO) //db에서 자동으로 생성되도록 해준다
+  @Column(name="user_id",nullable = false)
+  private UUID userId; //유저 식별번호
+
+  @Column(name="login_id")
+  private String loginId; //로그인 아이디
+
+  @Column
+  private String password;
+
+  @Column
+  private String role;
+
+  @OneToMany //조인할 대상 테이블의 컬럼이 있어야겠지요? 이게 바로 referencedColumnName
+  @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+  private List<UserLoginHist> userLoginHistList; //하나의 유저당 여러개의 로그인 이력을 가질 수 있음
+
+}
---
+++ src/main/java/group/oauth/feature/model/UserLoginHist.java
@@ -0,0 +1,48 @@
+package group.oauth.feature.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginHist {
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_login_hist_id", nullable = false)
+  private UUID userLoginHistId;
+
+  @Column(name = "user_id")
+  private UUID userId;
+
+  @Column
+  private DateTime loginDt;
+
+  public void defaultValue() {
+    this.loginDt = DateTime.now();
+  }
+
+  @PrePersist //db에 저장 전
+  public void onPrePersist() {
+    this.defaultValue();
+  }
+
+
+
+}
